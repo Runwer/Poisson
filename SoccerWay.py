1b45456e11c0546ef4f7dea9e8853f3c4a94c841
Runwer <runewerliin@runes-mbp.home>
2016-12-08 21:39:15 +0100

    Full crawl

--- SoccerWay.py
+++ SoccerWay.py
@@ -16,13 +16,21 @@ def match_data(soup, url):
     dict['date'] = Date(url[url.find('matches/')+8:url.find('/england')])
     if (Date(dict['date'])+85000) < datetime.now():
         dict['finalscore'] = soup.find('h3', {'class': 'thick scoretime '}).string.strip()
-        dict['scores'] = []
-
-        goals = soup.find('div', {'id': 'page_match_1_block_match_goals_13-wrapper'})
-
-        for g in goals.find_all('tr', {'class': 'event    expanded'}):
-            dict['scores'].append(
-                {g.find('span', {'class': 'minute'}).string: g.find('td', {'class': 'event-icon'}).string})
+        if dict['finalscore'] != '0 - 0':
+            dict['scores'] = []
+            try:
+                goals = soup.find('div', {'id': 'page_match_1_block_match_goals_13-wrapper'})
+                for g in goals.find_all('tr', {'class': 'event    expanded'}):
+                    dict['scores'].append(
+                        {g.find('span', {'class': 'minute'}).string: g.find('td', {'class': 'event-icon'}).string})
+            except:
+                try:
+                    goals = soup.find('div', {'id': 'page_match_1_block_match_goals_12-wrapper'})
+                    for g in goals.find_all('tr', {'class': 'event    expanded'}):
+                        dict['scores'].append(
+                            {g.find('span', {'class': 'minute'}).string: g.find('td', {'class': 'event-icon'}).string})
+                except:
+                    dict['scores'] = 'Missing'
 
         dict['teamH'] = soup.find('div', {'class': 'block_match_info real-content clearfix '}).find('h3', {
             'class': 'thick'}).findNext('a').string
@@ -34,23 +42,26 @@ def match_data(soup, url):
 
 def find_links(url, old_urls, todo_urls):
     new_urls = []
+    print url
     page = getpage(urlparse.urljoin('http://www.soccerway.com', url))
     soup = BeautifulSoup(page, "lxml")
     for tag in soup.findAll('a', href=True):
         tempTag = tag['href']
-        if '/?' in tempTag:
-            tempTag = tempTag[:tempTag.find('?')]
-        if ('england/premier-league' in tempTag or '/teams/england' in tempTag) and (tempTag not in old_urls and tempTag not in todo_urls) and all('/'+str(x)+'/' not in tempTag for x in match_db.keys()):
+        for t in ['/?', '/map/', '/venue/', '/head2head/', '/#events', '/commentary/']:
+            if t in tempTag:
+                tempTag = tempTag[:tempTag.find(t[1:])]
+        if ('england/premier-league' in tempTag or '/teams/england' in tempTag) and ('/statistics/' not in tempTag and '/venue' not in tempTag and '/transfer' not in tempTag) and (tempTag not in old_urls and tempTag not in todo_urls and tempTag not in new_urls) and all('/'+str(x)+'/' not in tempTag for x in match_db.keys()):
             new_urls.append(tempTag)
-    if '/matches/20' in url:
+    if '/matches/20' in url and '/head2head/' not in url and '/commentary/' not in url and '/map/' not in url and 'venue' not in url and '#events' not in url:
+        print url
         find_id = page.find('data-matchids="')
         matchid = page[find_id+15: page.find('"', find_id+15)]
         match_db[matchid] = match_data(soup, url)
     return new_urls
 
 url_crawled = json.loads(open('crawled.json').read())
-match_db = {}
-url_to_crawl = ['http://www.soccerway.com/matches/2016/12/03/england/premier-league/manchester-city-football-club/chelsea-football-club/2241892']#json.loads(open('to_crawl.json').read())
+match_db =  json.loads(open('match_db.json').read())
+url_to_crawl = json.loads(open('to_crawl.json').read())
 
 
 
@@ -72,7 +83,7 @@ while True:
             json.dump(url_to_crawl, outfile)
         with open('match_db.json', 'w') as fp:
             json.dump(match_db, fp)
-    if i > 1000:
+    if i > 100000:
         break
 
 
